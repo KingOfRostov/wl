9ac7c688249ca5e5513ce0942d1797dac40bc68a
Sergey Mkrtchyan <s.mkrtchyan@elonsoft.ru>
2021-01-02 18:10:03 +0300

    add wishes number to users

--- lib/wl/properties/commands/create_wish.ex
+++ lib/wl/properties/commands/create_wish.ex
@@ -1,10 +1,28 @@
 defmodule Wl.Properties.Commands.CreateWish do
+  alias Wl.Accounts.Entities.User
   alias Wl.Properties.Entities.Wish
   alias Wl.Repo
+  import Ecto.Query
 
   def process(attrs) do
-    %Wish{}
-    |> Wish.changeset(attrs)
-    |> Repo.insert()
+    transaction =
+      Repo.transaction(fn ->
+        res =
+          %Wish{}
+          |> Wish.changeset(attrs)
+          |> Repo.insert()
+
+        user_id = attrs["user_id"] || attrs[:user_id]
+
+        wish_user_query = from u in User, where: is_nil(u.archived_at) and u.id == ^user_id
+
+        Repo.update_all(wish_user_query, inc: [wishes_number: 1])
+        res
+      end)
+
+    case transaction do
+      {:ok, res} -> res
+      error -> error
+    end
   end
 end
