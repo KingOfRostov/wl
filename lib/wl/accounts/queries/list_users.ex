5d3dd53c629df2d1c9bf7631f4ddfd8664ddfa87
Sergey Mkrtchyan <s.mkrtchyan@elonsoft.ru>
2021-01-03 16:04:03 +0300

    preload and show preview last 3 user's wishes on users index page

--- lib/wl/accounts/queries/list_users.ex
+++ lib/wl/accounts/queries/list_users.ex
@@ -1,14 +1,28 @@
 defmodule Wl.Accounts.Queries.ListUsers do
   alias Wl.Accounts.Entities.User
+  alias Wl.Properties.Entities.Wish
   alias Wl.Accounts.Queries.Filters.UserFilter.Filter
   alias Wl.Repo
   import Ecto.Query
 
   def process(current_user_id, search_params) do
+    ranking_query =
+      from(w in Wish, as: :wish)
+      |> where([{:wish, w}], is_nil(w.archived_at))
+      |> select([{:wish, w}], %{id: w.id, row_number: row_number() |> over(:wishes_partition)})
+      |> windows(wishes_partition: [partition_by: :user_id, order_by: :inserted_at])
+
+    wish_subquery =
+      from w in Wish,
+        join: r in subquery(ranking_query),
+        on: w.id == r.id and r.row_number <= 3,
+        where: is_nil(w.archived_at)
+
     from(u in User, as: :user)
     |> where([{:user, u}], is_nil(u.archived_at))
     |> Filter.add_query(search_params)
     |> order_by([{:user, u}], [{:desc, u.id == ^current_user_id}, {:asc, u.inserted_at}])
+    |> preload([{:user, u}], wishes: ^wish_subquery)
     |> Repo.paginate(search_params)
   end
 
