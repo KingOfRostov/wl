d5b026d7dbff56272054c19a237c39a2a1f22659
Sergey Mkrtchyan <s.mkrtchyan@elonsoft.ru>
2021-01-04 12:55:25 +0300

    add more information on wishes index page

--- lib/wl/properties/queries/list_wishes.ex
+++ lib/wl/properties/queries/list_wishes.ex
@@ -9,6 +9,7 @@ defmodule Wl.Properties.Queries.ListWishes do
     |> where([{:wish, w}], is_nil(w.archived_at))
     |> Filter.add_query(search_params)
     |> order_by([{:wish, w}], {:asc, w.inserted_at})
+    |> preload(^Wish.preload_list())
     |> Repo.paginate(search_params)
   end
 end
