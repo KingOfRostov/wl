58e8c1799cee522974de8a727b93406020a24d8c
Sergey Mkrtchyan <s.mkrtchyan@elonsoft.ru>
2021-01-06 14:30:11 +0300

    add vue-material and remove useless files

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,18 +1,43 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="right-side-buttons">
+    <button class="btn btn-primary mx-1">Add wish</button>
+    <button class="btn btn-warning mx-1">Edit</button>
+  </div>
+  <div class="main-information">
+    Image
+  </div>
+  <div class="main-information">
+    <div>
+      <h2 class="username-text">{{ username }}</h2>
+    </div>
+    <div>
+      <h3 class="fulle-name-text">{{ name + ' ' + surname }}</h3>
+    </div>
+    <div>
+      <h3 class="fulle-name-text">Followers: {{ followers }}</h3>
+    </div>
+    <div>
+      <h3 class="fulle-name-text">Followed: {{ followed }}</h3>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  data() {
+    return {
+      username: '@king_of_rostov',
+      name: 'Сергей',
+      surname: 'Мкртчян',
+      followers: 0,
+      followed: 4,
+      wishes_number: 1
+    };
+  },
+  name: 'Home'
+};
 </script>
+
+<style scoped>
+
+</style>
